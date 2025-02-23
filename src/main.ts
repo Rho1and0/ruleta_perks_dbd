1fbd119b8aee3f6c62595bd1ee600d63b8bac74b
Rolando Monzon <rho1and0@hotmail.com>
2025-02-23 01:16:16 -0400

    instalando vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import 'vuetify/styles'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .use(store)
+  .use(router)
+  .mount('#app')
