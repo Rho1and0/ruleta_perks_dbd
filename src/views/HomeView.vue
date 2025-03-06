b8e3e5e7edeeaf892d721cf2e968a842db3a7a13
Rolando Monzon <rho1and0@hotmail.com>
2025-03-06 04:45:06 -0400

    finalizado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,36 +3,54 @@
     <v-app>
       <v-main>
         <v-container>
-          <Carousel ref="perk1" v-bind="carouselConfig" v-model="positions[0]">
-            <Slide v-for="(slide, idx) in perks" :key="idx">
-              <div class="perk-wrapper">
-                <img width="100px" height="100px" :src="slide.icon_name">
+          <v-row>
+            <v-col cols="3">
+              <div class="perks-wrapper">
+                <Carousel ref="perk1" v-bind="carouselConfig" v-model="positions[0]">
+                  <Slide v-for="(slide, idx) in perks" :key="idx">
+                    <div class="perk-wrapper">
+                      <img width="100px" height="100px" :src="slide.icon_name">
+                    </div>
+                  </Slide>
+                </Carousel>
               </div>
-            </Slide>
-          </Carousel>
-          <Carousel ref="perk2" v-bind="carouselConfig" v-model="positions[1]">
-            <Slide v-for="(slide, idx) in perks" :key="idx">
-              <div class="perk-wrapper">
-                <img width="100px" height="100px" :src="slide.icon_name">
+            </v-col>
+            <v-col cols="3">
+              <div class="perks-wrapper">
+                <Carousel ref="perk2" v-bind="carouselConfig" v-model="positions[1]">
+                  <Slide v-for="(slide, idx) in perks" :key="idx">
+                    <div class="perk-wrapper">
+                      <img width="100px" height="100px" :src="slide.icon_name">
+                    </div>
+                  </Slide>
+                </Carousel>
               </div>
-            </Slide>
-          </Carousel>
-          <Carousel ref="perk3" v-bind="carouselConfig" v-model="positions[2]">
-            <Slide v-for="(slide, idx) in perks" :key="idx">
-              <div class="perk-wrapper">
-                <img width="100px" height="100px" :src="slide.icon_name">
+            </v-col>
+            <v-col cols="3">
+              <div class="perks-wrapper">
+                <Carousel ref="perk3" v-bind="carouselConfig" v-model="positions[2]">
+                  <Slide v-for="(slide, idx) in perks" :key="idx">
+                    <div class="perk-wrapper">
+                      <img width="100px" height="100px" :src="slide.icon_name">
+                    </div>
+                  </Slide>
+                </Carousel>
               </div>
-            </Slide>
-          </Carousel>
-          <Carousel ref="perk4" v-bind="carouselConfig" v-model="positions[3]">
-            <Slide v-for="(slide, idx) in perks" :key="idx">
-              <div class="perk-wrapper">
-                <img width="100px" height="100px" :src="slide.icon_name">
+            </v-col>
+            <v-col cols="3">
+              <div class="perks-wrapper">
+                <Carousel ref="perk4" v-bind="carouselConfig" v-model="positions[3]">
+                  <Slide v-for="(slide, idx) in perks" :key="idx">
+                    <div class="perk-wrapper">
+                      <img width="100px" height="100px" :src="slide.icon_name">
+                    </div>
+                  </Slide>
+                </Carousel>
               </div>
-            </Slide>
-          </Carousel>
+            </v-col>
+          </v-row>
           <v-row justify-center>
-            <v-btn @mousedown="turnround" @mouseup="stopTurnround" @mouseleave="stopTurnround">SPIN</v-btn>
+            <v-btn @mousedown="turnround" @mouseup="stopTurnround" @mouseleave="stopTurnround">Jugar</v-btn>
           </v-row>
         </v-container>
       </v-main>
@@ -103,7 +121,10 @@ const perks = ref([])
 
 const showResult = ref(false)
 const intervalId = ref(null)
-const myCarousel = ref(null)
+const perk1 = ref(null)
+const perk2 = ref(null)
+const perk3 = ref(null)
+const perk4 = ref(null)
 const carouselConfig = {
   dir: 'btt',
   itemsToShow: 2,
@@ -115,32 +136,41 @@ const carouselConfig = {
 }
 
 // 3 carretes para la máquina tragamonedas
-const positions = ref([0, 1, 2, 4])
+const positions = ref([0, 1, 2, 3])
 
 onMounted(() => {
   loadPerksSurvis();
 })
 
+const randPerk = (INDEX) => {
+  const extraShift = Math.floor(Math.random() * (perks.value.length*3));
+  return (positions.value[INDEX] + extraShift) % perks.value.length;
+}
+
 // Función para iniciar el giro
 const spin = () => {
-  const extraShift = Math.floor(Math.random() * (perks.value.length*3));
-  let finalIndex = (positions.value[2] + extraShift) % perks.value.length;
-  console.log(positions.value[2], finalIndex);
+  const finalInds = [randPerk(0), randPerk(1), randPerk(2), randPerk(3)]
+
+  console.log(positions.value, finalInds);
   intervalId.value = setInterval(() => {
-    positions.value[2]++;
-    finalIndex--;
-    console.log('finalIndex', finalIndex);
-    if(finalIndex === 0) {
-      clearInterval(intervalId.value);
-      intervalId.value = null;
-      showResult.value = true;
-    }
+      finalInds.forEach( (val, INDEX) => {
+      positions.value[INDEX]++;
+      finalInds[INDEX]--;
+      if(finalInds[INDEX] === 0) {
+        clearInterval(intervalId.value);
+        intervalId.value = null;
+        showResult.value = true;
+      }
+    })
   }, 60);
 }
 
 const turnround = () => {
   intervalId.value = setInterval(() => {
+    positions.value[0]++;
+    positions.value[1]++;
     positions.value[2]++;
+    positions.value[3]++;
   }, 60); // Ajusta el tiempo a tu gusto (en milisegundos)
 }
 
@@ -156,6 +186,9 @@ const loadPerksSurvis = () => {
   axios.get('/perks.json')
     .then(response => {
       perks.value = response.data
+      setTimeout( () => {
+        positions.value = [randPerk(0), randPerk(1), randPerk(2), randPerk(3)]
+      }, 300)
     })
     .catch(error => {
       console.error('Error al cargar los datos:', error)
